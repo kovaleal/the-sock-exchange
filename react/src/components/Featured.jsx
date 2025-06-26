2783bb912d16abc8031d79bd260157229bd832b1
kovaleal <a.kovalenko1996@gmail.com>
2025-06-26 17:12:09 -0400

    added mongodb support

--- react/src/components/Featured.jsx
+++ react/src/components/Featured.jsx
@@ -4,13 +4,16 @@ const Featured = (props) => {
     return (
         <>
             <h5 className="card-title">Featured</h5>
-            <div className="card-container" style={{ display: 'flex', flexWrap: 'wrap', gap: "20px", padding: "20px" }}/>
-            {/*<div className="card-container d-flex flex-row justify-content-start" style={{ gap: "20px", padding: "20px" }}/>*/}
+            <div style={{padding: "10px"}}></div>
+            <div style={{display: "flex", gap: "0px", padding: "0px"}}>
+            <div className="card-container d-flex flex-row justify-content-start" style={{ gap: "0px", padding: "0px" }}/>
             {
                 props.data.map((promo) => (
                     <Promo key={promo.id} data={promo} />
                 ))
             }
+            </div>
+            <div style={{padding: "10px"}}></div>
         </>
     );
 };
